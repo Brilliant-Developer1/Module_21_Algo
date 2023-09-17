7b44b966f46653a4d41662b10618466b750e5153
Brilliant-Developer1 <email2obail@gmail.com>
2023-09-17 23:53:29 +0600

    first commit

---
+++ Fibonacci.cpp
@@ -0,0 +1,83 @@
+// #include<bits/stdc++.h>
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <complex>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <cwchar>
+#include <cwctype>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <random>
+#include <regex>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <strstream>
+#include <tuple>
+#include <typeinfo>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <valarray>
+#include <vector>
+
+using namespace std;
+
+int fibo(int n)
+{
+
+    if (n == 0 || n == 1)
+    {
+        return 1;
+    }
+
+    int ans1 = fibo(n - 1);
+    int ans2 = fibo(n - 2);
+    return ans1 + ans2;
+}
+
+int main()
+{
+
+    int n;
+    cin >> n;
+    cout << fibo(n) << endl;
+    return 0;
+}
