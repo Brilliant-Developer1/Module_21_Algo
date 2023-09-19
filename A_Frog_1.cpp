3725c22f87b451dfff61d5063a1c3442bcf68e49
Brilliant-Developer1 <email2obaidul@gmail.com>
2023-09-19 21:15:25 +0600

    memoization added

--- A_Frog_1.cpp
+++ A_Frog_1.cpp
@@ -58,28 +58,46 @@
 #include <valarray>
 #include <vector>
 
-#define ll long long
+// #include<bits/stdc++.h>
 using namespace std;
+const int N = 1e5 + 5;
+int dp[N];
+
+int solve(int a[], int n)
+{
+    if (n == 1)
+    {
+        return 0;
+    }
+
+    if (dp[n] != -1)
+        return dp[n];
+
+    else if (n == 2)
+    {
+        return dp[n] = abs(a[2] - a[1]);
+    }
+    else
+    {
+        int choice1 = solve(a, n - 1) + abs(a[n] - a[n - 1]);
+        int choice2 = solve(a, n - 2) + abs(a[n] - a[n - 2]);
+
+        return dp[n] = min(choice1, choice2);
+    }
+}
 
 int main()
 {
 
-    ll n;
+    int n;
     cin >> n;
-
-    ll a[n + 1];
-    a[0] = 1;
-    a[1] = 1;
-
-    for (int i = 2; i <= n; i++)
+    int a[n + 1];
+    for (int i = 1; i <= n; i++)
     {
-        a[i] = a[i - 1] + a[i - 2];
+        dp[i] = -1;
+        cin >> a[i];
     }
-
-    for (int i = 0; i <= n; i++)
-    {
-        cout << a[i] << " ";
-    }
+    cout << solve(a, n);
 
     return 0;
 }
