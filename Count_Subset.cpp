d355f3e04cca48a41f4d0d535109fb6067300a59
Brilliant-Developer1 <email2obaidul@gmail.com>
2023-09-23 18:39:29 +0600

    Count_Subset added

---
+++ Count_Subset.cpp
@@ -0,0 +1,115 @@
+// #include<bits/stdc++.h>
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <complex>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <cwchar>
+#include <cwctype>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <random>
+#include <regex>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <strstream>
+#include <tuple>
+#include <typeinfo>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <valarray>
+#include <vector>
+
+using namespace std;
+
+int dp[1005][1005];
+
+int subset_sum(int n, int a[], int s)
+{
+    if (n == 0)
+    {
+        if (s == 0)
+            return 1;
+        else
+            return 0;
+    }
+    if (dp[n][s] != -1)
+    {
+        return dp[n][s];
+    }
+
+    if (a[n - 1] <= s)
+    {
+        int op1 = subset_sum(n - 1, a, s - a[n - 1]);
+        int op2 = subset_sum(n - 1, a, s);
+
+        return dp[n][s] = op1 + op2;
+    }
+    else
+    {
+        return dp[n][s] = subset_sum(n - 1, a, s);
+    }
+}
+
+int main()
+{
+
+    int n;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    int s;
+    cin >> s;
+
+    for (int i = 0; i <= n; i++)
+    {
+        for (int j = 0; j <= s; j++)
+        {
+            dp[i][j] = -1;
+        }
+    }
+
+    cout << subset_sum(n, a, s);
+    return 0;
+}
